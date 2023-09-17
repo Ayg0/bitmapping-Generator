0c9495b5f4e741abbe0a3c11c504a29daaa4e7ae
Taha Ed-dafili <ted-dafi@c4r1p2.1337.ma>
2023-09-17 16:27:19 +0100

    wow dynamic grid using js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 var a = 0;
-
 function funct() {
     a++;
     document.getElementById("clicks").innerHTML = a;
+}
+
+function changeme(event) {
+    var elementId = event.target.id;
+    let elementStyle = document.getElementById(elementId).style;
+    if (elementStyle.backgroundColor == "rgb(122, 183, 178)"){
+        elementStyle.backgroundColor = "rgb(171, 228, 223)";
+        elementStyle.margin = "0px";
+    }
+    else{
+        elementStyle.backgroundColor = "rgb(122, 183, 178)";
+        elementStyle.margin = "3px";
+    }
+}
+function creategrid(){
+    let x = document.getElementById("ix").value;
+    let y = document.getElementById("iy").value;
+    let n = 0;
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+    container.style.gridTemplateColumns = "repeat(" + x + ", 1fr)";
+    container.style.gridTemplateRows = "repeat(" + y + ", 1fr)";
+    for (var rows = 0; rows < y; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            container.innerHTML += '<div class="item item'+ n +'" id="item'+ n + '" onclick="changeme(event)"></div>';
+            n++;
+        };
+        n++;
+    };
 }
